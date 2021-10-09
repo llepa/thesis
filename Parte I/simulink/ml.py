76487c8b6f93ca923aa986855b493af054ae3dd4
Andrei Laurentiu <and.lepadat@gmail.com>
2021-10-09 17:07:42 +0200

    start working on apollo

--- Parte I/simulink/ml.py
+++ Parte I/simulink/ml.py
@@ -22,7 +22,6 @@ class SimulinkPlant:
         self.out = dict()
 
     def setValue(self, varName, value):
-
         # Helper function to set value of control action
         # self.eng.set_param(self.handle, varName, str(value), nargout=0)
         self.eng.set_param(self.modelName + '/' + varName, 'Value', str(value), nargout=0)
@@ -37,7 +36,6 @@ class SimulinkPlant:
 
 
     def getValue(self, value):
-
         # Helper function to get plant output and time 
         try:
             return self.eng.eval(value)
@@ -57,14 +55,13 @@ class SimulinkPlant:
 
         # Loading the model
         print('Loading model...')
-
         self.eng.eval('model = "{}"'.format(self.modelPath), nargout=0)
         self.handle = self.eng.eval('load_system(model)')
         print('Model loaded')
         print('Connected')
 
+    '''
     def intializeGraph(self):
-
         print('Initializing graph...')
         plt.close()
         plt.ion()
@@ -76,16 +73,15 @@ class SimulinkPlant:
         plt.pause(0.1)
         print('Graph initialized')
 
-
     def updateGraph(self):
-
+    
         self.fig.set_xdata(self.out['tout'])
         self.fig.set_ydata(self.out['output'])
         #self.fig.set_ydata(self.out['simout'])
 
         plt.pause(0.1)
         plt.show()
-
+    '''
 
     def print(self):
         graphString = ""
@@ -96,37 +92,30 @@ class SimulinkPlant:
             legendString += "\'" + val + "\',"
         printString = graphString[0: len(graphString)-1]
         legendString = legendString[0: len(legendString)-1]
-        #self.eng.eval('f1 = figure')
         self.eng.eval('plot(' + printString + ')')
         self.eng.eval('legend({' + legendString + '})')
-        #self.eng.eval('print("f1", "-djpeg")')
 
 
     def simulate(self, introduceError):
+        t = 1                   # the simulation stops 25 times per second
+        sampleTime = 3         
 
-        t = 0.04             # the simulation stops 25 times per second
-        sampleTime = 0.04    # second(s), chosen in [0.04, x], where x is the stop time of the simulation
-        epsilon = 0.000000005
         self.eng.set_param(self.handle, 'SimulationCommand', 'start', 'SimulationCommand', 'pause', nargout=0)
-
-        #print('Value of ' + 'throttle.signals.values' + 'before starting simulation: ' + str(self.getValue('throttle.signals.values')))
 
         if (introduceError):
             while (self.eng.get_param(self.handle, 'SimulationStatus') != ('stopped' or 'terminating')):     
-                #print("Time: " + str(t))
-                # OVVIAMENTE CORREGGERE QUESTO OBROBRIO QUI SOTTO !!!
-                if (True):
+                if (not t % sampleTime):
                     for value in self.noiseValues:
                         #nominalVal = np.array(self.getLastValue(value[0]))[-1]
                         #nominalVal.astype(float)
-                        r = random.random() - 0.5 
+                        r = random.random() / 0.5 - 0.5     # based just on a random value but can be implemented in a more complex way
                         self.setValue(value[1], str(r))
                 '''
                 else:
                     for value in self.noiseValues:
                         self.setValue(value[1], str(0))
                 '''
-                t += 0.04
+                t += 1
                 self.eng.set_param(self.handle, 'SimulationCommand', 'continue', 'SimulationCommand', 'pause', nargout=0) 
         else:
             self.eng.set_param(self.handle, 'SimulationCommand', 'continue', nargout=0)
@@ -143,7 +132,7 @@ class SimulinkPlant:
 
         s = ''
         while(s != 'y'):
-            s = input('Do you want to finish the simulation? [y/n]: ')
+            s = input('Do you want to end the simulation? [y/n]: ')
         
         print('Simulation ended')
 
